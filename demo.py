0d16aa93da330e6849f979cacf8f3852250ce7b7
Zhen Li <liz6@ferris.edu>
2024-08-25 10:50:38 -0400

    adding some example

--- demo.py
+++ demo.py
@@ -1,3 +1,21 @@
 import streamlit as st
 
 st.title("The first streamlit app!")
+
+st.header("_Streamlit_ is :green[FUN] :rocket: :star2: :hand:")
+st.header("This is a header with a divider", divider="rainbow")
+st.header("These headers have rotating dividers", divider=True)
+st.header("One", divider=True)
+st.markdown("* _Step 1_")
+st.markdown("* _Step 2_")
+
+st.header("Two", divider=True)
+col1, col2 = st.columns(2)
+with col1:
+    x = st.slider("Choose and x value", 1,100)
+with col2:
+    st.write("the square of :red[**x**]is: ", x*x)
+
+st.header("Three", divider=True)
+
+st.header("Four", divider=True)
